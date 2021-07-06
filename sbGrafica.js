5d122be9c82b5253b5839b848421288f53253a5c
paatto1 <86998472+paatto1@users.noreply.github.com>
2021-07-06 12:15:22 -0500

    Add files via upload

---
+++ sbGrafica.js
@@ -0,0 +1,299 @@
+$(document).ready(function() {
+  var db = firebase.database();
+  var coleccionPais = db.ref().child("pais");    
+  var coleccionSucursal=db.ref().child("sucursal"); 
+  var coleccionCanal=db.ref().child("canal");     
+  var coleccionOpcion=db.ref('opcion');
+  var coleccionEncuesta=db.ref().child("encuesta");    
+  var Pais,Ciudad,Canal,Pregunta,Opcion,Comentario;
+    
+  var Preguntas = [] ;  
+  var PreguntasID = [] ;       
+  var Opciones=[];
+  var Encuestas=[];
+    
+var Excelente=[0,0,0,0];
+var Bueno=[0,0,0,0];
+var Regular=[0,0,0,0];
+var Malo=[0,0,0,0];
+    
+var Tabulacion = [
+     [0,0,0,0],
+     [0,0,0,0],
+     [0,0,0,0],
+     [0,0,0,0]
+];
+    
+    
+     var myChart; 
+    
+ $("#cbopais").empty();
+    
+ coleccionPais.on("value", function(data) { 
+    
+     data.forEach(function(e){
+      var row = e.val();
+       key= e.key? e.key:'--'; 
+       descripcion= row['pais'] ? row['pais']:'--';        
+       $('#cbopais').prepend("<option value="+ key + ">"+descripcion+"</option>");
+  });
+      Pais= $('#cbopais').val();  
+        
+});   
+
+ $('#cbocanal').empty();
+    
+ $('#cbocanal').on('change',function(){    
+    Canal=$('#cbocanal').val();     
+});       
+
+ coleccionCanal.on("value", function(data) {     
+     data.forEach(function(e){
+      var row = e.val();
+       key= e.key? e.key:'--'; 
+       descripcion= row['canal'] ? row['canal']:'--';        
+       $('#cbocanal').prepend("<option value="+ key + ">"+descripcion+"</option>");
+  });  
+        Canal=$('#cbocanal').val(); 
+});   
+    
+ $("#cbosucursal").empty();  
+ 
+ coleccionSucursal.on("value", function(data) {     
+ data.forEach(function(e){
+  var row = e.val();
+   key= e.key? e.key:'--'; 
+   descripcion= row['descripcion'] ? row['descripcion']:'--'; 
+   pais= row['pais'] ? row['pais']:'--';      
+   if (pais==Pais) {
+    $('#cbosucursal').prepend("<option value="+ key + ">"+descripcion+"</option>");
+   }
+ });  
+});        
+
+ $('#cbopais').on('change',function(){    
+ Pais= $('#cbopais').val();  
+  $("#cbosucursal").empty();
+    
+  
+ coleccionSucursal.on("value", function(data) {     
+ data.forEach(function(e){
+  var row = e.val();
+   key= e.key? e.key:'--'; 
+   descripcion= row['descripcion'] ? row['descripcion']:'--'; 
+   pais= row['pais'] ? row['pais']:'--';      
+   if (pais==Pais) {
+    $('#cbosucursal').prepend("<option value="+ key + ">"+descripcion+"</option>");
+   }
+ });     
+     Ciudad=$('#cbosucursal').val();
+
+});    
+           
+}); 
+ 
+ $(document).on('click','#btnenviar',function(){  
+
+    Canal= $('#cbocanal').val();   
+    
+    coleccionPreguntas=coleccionCanal.child(Canal).child('Preguntas');      
+
+     $('.grafica').empty();
+     $('.grafica').append( '<canvas id="myChart" style="position: relative; height:30vh; width:70vw"></canvas>');
+     $('.grafica').append('<div class="PreguntasLabel">');
+     
+     
+    coleccionPreguntas.on("value", function(data) {    
+    Preguntas=[];
+    PreguntasID=[];
+     data.forEach(function(e){
+      var row = e.val();
+         key= e.key? e.key:'--'; 
+         pregunta= row[key] ? row[key]:'--';
+         var texto=row['item'];   
+        Preguntas.push(pregunta) ;
+        PreguntasID.push(key);  
+});
+    
+     
+    Preguntas.forEach(function(elemento, indice, array) {
+    $('.PreguntasLabel').append('<div>'+PreguntasID[indice]+'.'+elemento+'</div>');
+});
+ });
+    
+    coleccionEncuesta.on("value", function(data) {       
+    var ps1=$("#cbopais").val();
+    var cd1=$("#cbosucursal").val();
+    var cl1=$("#cbocanal").val();   
+    Excelente=[0,0,0,0];
+        Bueno=[0,0,0,0];
+      Regular=[0,0,0,0];
+         Malo=[0,0,0,0];
+    
+Tabulacion = [
+     [0,0,0,0],
+     [0,0,0,0],
+     [0,0,0,0],
+     [0,0,0,0],
+];
+    
+    data.forEach(function(e){       
+         var row = e.val();             
+         key= e.key? e.key:'--'; 
+         var pg=row['Pregunta'];
+         var op=row['Opcion'];  
+         var ps=row['Pais'];        
+         var cd=row['Ciudad'];
+         var cl=row['Canal'];         
+     
+         
+    if((ps==ps1) && (cd==cd1) && (cl==cl1)){      
+        
+         var p=1;
+         
+         while(p<=4){
+          if(pg==p){
+             
+            if(op==1){
+            Excelente[p-1]=Excelente[p-1]+1; 
+            Tabulacion[0][p-1]= Tabulacion[0][p-1]+1;
+            
+            }
+              
+                       
+           if(op==2){
+            Bueno[p-1]=Bueno[p-1]+1;   
+            Tabulacion[1][p-1]= Tabulacion[1][p-1]+1;
+           }
+              
+              
+           if(op==3){
+            Regular[p-1]=Regular[p-1]+1; 
+            Tabulacion[2][p-1]= Tabulacion[2][p-1]+1;   
+           }
+                
+        
+           if(op==4){
+            Malo[p-1]=Malo[p-1]+1; 
+            Tabulacion[3][p-1]= Tabulacion[3][p-1]+1;
+           }
+         }
+            p=p+1;
+        } 
+    };         
+      
+});
+
+});
+     
+var ctx = document.getElementById('myChart').getContext('2d');     
+
+  myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: PreguntasID, //preguntas
+        datasets: [{
+              
+            label: 'Excelente',//opcion
+            data: Tabulacion[0],//Excelente, //frecuencia PREGUNTA
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 99, 132)',
+                'rgb(255, 99, 132)',
+                'rgb(255, 99, 132)'
+              
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 100, 1)',
+             
+            ],
+            borderWidth: 1
+        },
+                   
+         {
+            
+            label: 'Bueno',
+            data: Tabulacion[1],
+            backgroundColor: [
+                  'rgb(0, 0, 255)',
+                  'rgb(0, 0, 255)',
+                  'rgb(0, 0, 255)',
+                  'rgb(0, 0, 255)'
+           
+            ],
+            borderColor: [
+                'rgba(200, 99, 132, 1)',
+                'rgba(200, 162, 235, 1)',
+                'rgba(200, 206, 86, 1)',
+                'rgba(200, 192, 192, 1)',
+              
+            ],
+            borderWidth: 1
+        },
+        
+        {     
+            label: 'Regular',//opcion
+            data: Tabulacion[2], //frecuencia PREGUNTA
+            backgroundColor: [
+                'rgb(0, 255, 0)',
+                'rgb(0, 255, 0)',
+                'rgb(0, 255, 0)',
+                'rgb(0, 255, 0)'
+          
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
+             
+            ],
+            borderWidth: 1
+        },
+                   
+        {    
+            label: 'Malo',//opcion
+            data: Tabulacion[3], //frecuencia PREGUNTA
+            backgroundColor: [
+                  'rgb(75, 0, 100)',
+                  'rgb(75, 0, 100)',
+                  'rgb(75, 0, 100)',
+                  'rgb(75, 0, 100)'
+          
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+             
+            ],
+            borderWidth: 1
+        },           
+                  
+        ]
+    },
+    
+    
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});    
+
+coleccionOpcion.on("value",function(d){            
+         d.forEach(function(e){
+              k=e.key;  
+              fila=e.val();   
+              Opciones.push(fila[k]);              
+       });    
+     }); 
+
+ });
+});  
---
+++ sbSucursal.js
@@ -0,0 +1,365 @@
+$(document).ready(function() {
+  var db = firebase.database();
+  var coleccionPais = db.ref().child("pais");    
+  var coleccionSucursal=db.ref().child("sucursal"); 
+  var coleccionCanal=db.ref().child("canal");     
+  var coleccionOpcion=db.ref('opcion');
+  var coleccionEncuesta=db.ref().child("encuesta");    
+  var Pais,Ciudad,Canal,Pregunta,Opcion,Comentario;    
+    
+    
+ 
+    
+ $("#cbopais").empty();
+    
+ coleccionPais.on("value", function(data) { 
+    
+     data.forEach(function(e){
+      var row = e.val();
+       key= e.key? e.key:'--'; 
+       descripcion= row['pais'] ? row['pais']:'--';        
+       $('#cbopais').prepend("<option value="+ key + ">"+descripcion+"</option>");
+  });
+      Pais= $('#cbopais').val();  
+        
+});   
+
+ $('#cbocanal').empty();
+    
+ $('#cbocanal').on('change',function(){    
+    Canal=$('#cbocanal').val();     
+});       
+
+ coleccionCanal.on("value", function(data) {     
+     data.forEach(function(e){
+      var row = e.val();
+       key= e.key? e.key:'--'; 
+       descripcion= row['canal'] ? row['canal']:'--';        
+       $('#cbocanal').prepend("<option value="+ key + ">"+descripcion+"</option>");
+  });  
+        Canal=$('#cbocanal').val(); 
+});   
+    
+ $("#cbosucursal").empty();  
+ 
+ coleccionSucursal.on("value", function(data) {     
+ data.forEach(function(e){
+  var row = e.val();
+   key= e.key? e.key:'--'; 
+   descripcion= row['descripcion'] ? row['descripcion']:'--'; 
+   pais= row['pais'] ? row['pais']:'--';      
+   if (pais==Pais) {
+    $('#cbosucursal').prepend("<option value="+ key + ">"+descripcion+"</option>");
+   }
+ });  
+});        
+
+ $('#cbopais').on('change',function(){    
+ Pais= $('#cbopais').val();  
+  $("#cbosucursal").empty();
+    
+  
+ coleccionSucursal.on("value", function(data) {     
+ data.forEach(function(e){
+  var row = e.val();
+   key= e.key? e.key:'--'; 
+   descripcion= row['descripcion'] ? row['descripcion']:'--'; 
+   pais= row['pais'] ? row['pais']:'--';      
+   if (pais==Pais) {
+    $('#cbosucursal').prepend("<option value="+ key + ">"+descripcion+"</option>");
+   }
+ });     
+     Ciudad=$('#cbosucursal').val();
+
+});    
+           
+});   
+
+ function writeEncuesta(pais, ciudad, canal, pregunta,opcion,comentario,clienteid) {
+     var newEncuesta = coleccionEncuesta.push();    
+      var id=newEncuesta.key;      
+     $('.opcion').children('#'+Pregunta).val(id);
+    
+    newEncuesta.set({
+                Pais: pais,
+                Ciudad: ciudad,
+                Canal: canal,
+                Pregunta:pregunta,
+                Opcion:opcion,
+                Comentario:comentario,
+                Clienteid:clienteid
+ });
+    
+    
+    //var newEncuesta = db.ref('encuesta').Push().key;    
+/*var newEncuesta = coleccionEncuesta.push();
+newEncuesta.set({
+                username: name,
+                email: email,
+                  profile_picture : imageUrl
+});*/
+    
+    
+   /* db.ref('encuesta/'+newEncuesta +'/Pais').set(Pais);
+    db.ref('encuesta/'+newEncuesta +'/Ciudad').set(Ciudad);
+    db.ref('encuesta/'+newEncuesta +'/Canal').set(Canal);    
+    db.ref('encuesta/'+newEncuesta +'/Pregunta').set(Preguta);   
+    db.ref('encuesta/'+newEncuesta +'/Opcion').set(Opcion);   
+    db.ref('encuesta/'+newEncuesta +'/Comentario').set(Comentario);  
+    db.ref('encuesta/'+newEncuesta +'/Clienteid').set(Clienteid);  */  
+}  
+    
+ function editEncuesta(pais, ciudad, canal, pregunta,opcion,comentario,clienteid,EncuestaId){
+    data={
+             'Pais': pais,
+             'Ciudad': ciudad,
+             'Canal': canal,
+             'Pregunta':pregunta,
+             'Opcion':opcion,
+             'Comentario':comentario,
+             'Clienteid':clienteid
+          };    
+    
+    var edit= coleccionEncuesta.child(EncuestaId);
+    edit.update(data);  
+    
+}    
+    
+    
+$(document).on('click','.1',function(){  
+  Pais=$('#cbopais').val();
+  Ciudad=$('#cbosucursal').val();
+  Canal=$('#cbocanal').val();
+  Pregunta=$(this).parent().attr('id');      
+  Opcion='1';
+  Clienteid='C01';  
+  Comentario=  $('.opcion').parent().find('#c'+Pregunta).val();
+ 
+  codigo= $(this).parent().find('#'+Pregunta).val();   
+  
+    
+
+   padre=$(this).parent().attr('id');
+
+    
+      coleccionOpcion.on("value",function(d){
+             d.forEach(function(e){
+             k=e.key;  
+             fila=e.val();   
+             var imagen=fila['img']; 
+             $('div#'+padre+'.opcion').find('.'+k).attr('src',imagen);
+             
+           }); 
+        });
+     
+    
+    
+  $(this).attr('src','https://raw.githubusercontent.com/paatto1/Streetburger/main/images/jv_exelente_activo.png');
+    
+  num=codigo.length;
+  
+  if(num==0){     
+    writeEncuesta(Pais, Ciudad, Canal, Pregunta,Opcion,Comentario,Clienteid)
+  }
+    
+ if(num>0){   
+      
+    editEncuesta(Pais, Ciudad, Canal, Pregunta,Opcion,Comentario,Clienteid,codigo);
+  }    
+    
+});  
+    
+
+$(document).on('click','.2',function(){
+  Pais=$('#cbopais').val();
+  Ciudad=$('#cbosucursal').val();
+  Canal=$('#cbocanal').val();
+  Pregunta=$(this).parent().attr('id');      
+  Opcion='2' ;
+  Clienteid='C01';  
+  Comentario=  $('.opcion').parent().find('#c'+Pregunta).val();
+  codigo= $(this).parent().find('#'+Pregunta).val();
+  
+    padre=$(this).parent().attr('id');
+
+    
+   padre=$(this).parent().attr('id');
+
+    
+      coleccionOpcion.on("value",function(d){
+             d.forEach(function(e){
+             k=e.key;  
+             fila=e.val();   
+             var imagen=fila['img']; 
+             $('div#'+padre+'.opcion').find('.'+k).attr('src',imagen);
+             
+           }); 
+        });
+     
+    
+    
+  $(this).attr('src','https://raw.githubusercontent.com/paatto1/Streetburger/main/images/jv_bueno_activo.png');
+        
+    num=codigo.length;   
+
+  
+  if(num==0){     
+    writeEncuesta(Pais, Ciudad, Canal, Pregunta,Opcion,Comentario,Clienteid)
+  }
+    
+ if(num>0){   
+    editEncuesta(Pais, Ciudad, Canal, Pregunta,Opcion,Comentario,Clienteid,codigo);
+  }   
+    
+});  
+    
+    
+$(document).on('click','.3',function(){
+  Pais=$('#cbopais').val();
+  Ciudad=$('#cbosucursal').val();
+  Canal=$('#cbocanal').val();
+  Pregunta=$(this).parent().attr('id');      
+  Opcion='3';   
+  Clienteid='C01';  
+  Comentario=  $('.opcion').parent().find('#c'+Pregunta).val();
+  codigo= $(this).parent().find('#'+Pregunta).val();
+ 
+    
+   padre=$(this).parent().attr('id');
+
+    
+      coleccionOpcion.on("value",function(d){
+             d.forEach(function(e){
+             k=e.key;  
+             fila=e.val();   
+             var imagen=fila['img']; 
+             $('div#'+padre+'.opcion').find('.'+k).attr('src',imagen);
+             
+           }); 
+        });
+     
+    
+    
+  $(this).attr('src','https://raw.githubusercontent.com/paatto1/Streetburger/main/images/jv_regular_activo.png');
+
+        
+    
+num=codigo.length;
+    
+
+  
+  if(num==0){     
+    writeEncuesta(Pais, Ciudad, Canal, Pregunta,Opcion,Comentario,Clienteid)
+  }
+    
+ if(num>0){   
+    editEncuesta(Pais, Ciudad, Canal, Pregunta,Opcion,Comentario,Clienteid,codigo);
+  }
+                    
+});  
+    
+    
+$(document).on('click','.4',function(){
+  Pais=$('#cbopais').val();
+  Ciudad=$('#cbosucursal').val();
+  Canal=$('#cbocanal').val();
+  Pregunta=$(this).parent().attr('id');      
+  Opcion='4' ; 
+  Clienteid='C01';  
+  Comentario=  $('.opcion').parent().find('#c'+Pregunta).val();
+   codigo= $(this).parent().find('#'+Pregunta).val();
+  
+   padre=$(this).parent().attr('id');
+
+    
+      coleccionOpcion.on("value",function(d){
+             d.forEach(function(e){
+             k=e.key;  
+             fila=e.val();   
+             var imagen=fila['img']; 
+             $('div#'+padre+'.opcion').find('.'+k).attr('src',imagen);
+             
+           }); 
+        });
+     $(this).attr('src','https://raw.githubusercontent.com/paatto1/Streetburger/main/images/jv_malo_activo.png');
+        
+    
+num=codigo.length;
+    
+
+  
+  if(num==0){     
+    writeEncuesta(Pais, Ciudad, Canal, Pregunta,Opcion,Comentario,Clienteid)
+  }
+    
+ if(num>0){ 
+     console.log(codigo);
+    editEncuesta(Pais, Ciudad, Canal, Pregunta,Opcion,Comentario,Clienteid,codigo);
+  }  
+      
+});        
+        
+var imagenes='';
+imagenes='<div class="opcion">'; 
+        
+coleccionOpcion.on("value",function(d){
+     d.forEach(function(e){
+     k=e.key;     
+     fila=e.val();   
+      var imagen=fila['img']; 
+         console.log(k);
+         imagenes=imagenes+'<img src="'+ imagen +'" class="'+k+'">';   
+      });             
+});
+         
+imagenes=imagenes+'</div>';  
+    
+    
+$('#btnenviar').on('click',function(){   
+     $('.pregunta').remove(); 
+     $('.opcion').remove(); 
+  coleccionPreguntas=coleccionCanal.child(Canal).child('Preguntas');    
+  coleccionPreguntas.on("value", function(data) {
+     data.forEach(function(e){
+      var row = e.val();
+         key= e.key? e.key:'--'; 
+         pregunta= row[key] ? row[key]:'--';
+         var texto=row['item'];
+         
+      $('#preguntas').append('<div class="pregunta" id="'+key+'"><strong>'+ pregunta+'</strong></div>');
+       
+         
+        imagenes='';
+        imagenes='<div class="opcion" id="'+key+'">';
+        
+         coleccionOpcion.on("value",function(d){
+             d.forEach(function(e){
+              k=e.key;  
+              k1='img'+k;
+              fila=e.val();   
+              var imagen=fila['img'];  
+                imagenes=imagenes+'<img src="'+ imagen +'" class="'+k+'">';   
+             
+           });             
+             
+         });
+         
+     imagenes=imagenes+'<input type="hidden" class="codigo" id="'+key+'"></div>';      
+         
+
+      $('#preguntas').append(imagenes);
+        
+        var cm='c'+key;
+      
+$('#preguntas').append('<div class="opcion"><textarea class="comentario" id="'+cm+'"  placeholder="'+ texto +'" cols="40" rows="3" class="form-control"></textarea></div>');    
+  });
+ }); 
+    $('#preguntas').append('<div class="opcion"><button class="finalizar">Finalizar</button></div>'); 
+});   
+   
+ 
+    $(document).on("click", ".finalizar", function() { 
+       $('#content').load('sbFinalizar.html');
+    });    
+    
+});
