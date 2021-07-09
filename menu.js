cacb0ca6bca70b9c578a59b0b871818ce0fff8c1
paatto1 <86998472+paatto1@users.noreply.github.com>
2021-07-08 22:58:34 -0500

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,23 +1,20 @@
-$(document).ready(main);
+$(document).ready(function(){
 
-var contador = 1;
-
-function main(){
 	$('.menu_bar').click(function(){
-		// $('nav').toggle(); 
-
-		if(contador == 1){
-			$('nav').animate({
+	
+        if ($('nav').is(':hidden')){
+            $('nav').show();
+            	$('nav').animate({
 				left: '0'
-			});
-			contador = 0;
-		} else {
-			contador = 1;
-			$('nav').animate({
-				left: '-100%'
-			});
-		}
-
-	});
-
-};
+			}); 
+        }else{
+           $('nav').hide();
+          }
+        
+         console.log($('nav').css('display'));
+        }) ;
+        
+        
+       
+        
+});
